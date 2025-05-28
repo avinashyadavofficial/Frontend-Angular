3d3951eddd1f6b7486e3bb779bcc5ea5d1f978f4
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-28 11:45:38 +0530

    [Avinash Yadav]:Day1: Add javascript and typescript file to implement enum in typescript

---
+++ Day1/enum.ts
@@ -0,0 +1,14 @@
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday
+}
+
+let today: Day = Day.Monday;
+
+console.log(today);        //0
+console.log(Day[today]);   //Monday
