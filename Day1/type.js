90fcca116dd13240611e1708bc8c20cb05b42194
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-28 11:45:18 +0530

    [Avinash Yadav]:Day1: Add javascript and typescript file to implement type in typescript

---
+++ Day1/type.js
@@ -0,0 +1,8 @@
+// Using the type
+var person = {
+    name: "Avinash",
+    age: 22,
+    isStudent: false,
+};
+console.log(person.name); // Output: Alice
+console.log(person.age); // Output: 25
---
+++ Day1/type.ts
@@ -0,0 +1,14 @@
+type Person = {
+  name: string;
+  age: number;
+  isStudent: boolean;
+};
+
+const person: Person = {
+  name: "Avinash",
+  age: 22,
+  isStudent: false,
+};
+
+console.log(person.name); // Avinash
+console.log(person.age);  // 22
