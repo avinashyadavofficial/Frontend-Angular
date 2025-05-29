1300b97a76cc34ff5ca8066d0bfbcb43ff8cc8a8
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-29 21:25:44 +0530

    Implement task completion functionality with event emission and filtering

--- Day4/task_management application/src/app/tasks/task/task.component.ts
+++ Day4/task_management application/src/app/tasks/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input } from '@angular/core';
+import { Component,Input, Output ,EventEmitter} from '@angular/core';
 import { type Task } from './task.model';
 @Component({
   selector: 'app-task',
@@ -8,4 +8,9 @@ import { type Task } from './task.model';
 })
 export class TaskComponent {
    @Input({required:true}) task!:Task;
+   @Output() complete=new EventEmitter<string>();
+
+   onCompleteTask(){
+    this.complete.emit(this.task.id);
+   }
 }
