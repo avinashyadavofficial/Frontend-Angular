635f76f16a2e057ca56e46fdcc3f854df7cd0446
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-29 16:38:32 +0530

    Implemented ngFor and ngIf

--- Day4/task_management application/src/app/app.component.ts
+++ Day4/task_management application/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from "./dumy-users";
+import { NgFor, NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent,TasksComponent],
+  imports: [HeaderComponent, UserComponent,TasksComponent,NgFor,NgIf],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']  
 })
