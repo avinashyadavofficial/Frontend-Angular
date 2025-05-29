378eb1fec76156fb0f6d6c0cec821637d3254053
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-29 17:03:21 +0530

    Create task component inside tasks component

--- Day4/task_management application/src/app/app.component.ts
+++ Day4/task_management application/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from "./dumy-users";
-import { NgFor, NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent,TasksComponent,NgFor,NgIf],
+  imports: [HeaderComponent, UserComponent,TasksComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']  
 })
