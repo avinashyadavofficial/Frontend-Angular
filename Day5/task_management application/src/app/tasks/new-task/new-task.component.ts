887194b7a9de5ef61ad00679d34719a358e6d884
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-30 10:31:19 +0530

    [Avinash Yadav]: Add Task Management Application (Continuation)

---
+++ Day5/task_management application/src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,31 @@
+import { Component, Output,EventEmitter} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task/task.model';
+
+@Component({
+  selector: 'app-new-task',
+  imports: [FormsModule],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css'
+})
+export class NewTaskComponent {
+  @Output() cancel=new EventEmitter<void>();
+  @Output() add=new EventEmitter<{title: string; summary: string; dueDate: string}>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDueDate = '';
+  @Output() addTask = new EventEmitter<NewTaskData>();
+
+  onCancel(){
+   this.cancel.emit();
+ }
+
+ onSubmit(){
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      dueDate: this.enteredDueDate
+    });
+ }
+}
+ 
---
+++ Day5/task_management application/src/app/user/user.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, Output, EventEmitter ,output} from '@angular/core';
+import { User } from './user.model';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({required:true}) user!:User;
+  @Input({required:true}) selected!:boolean;
+  @Output() select=new EventEmitter();
+  // select=output<string>();
+  get imagePath(){
+    return '/users/'+this.user.avatar;
+  }
+  onSelectUser(){
+    this.select.emit(this.user.id);
+  }
+}
+ 
