410353c6453427cd728f1fdacecd92ca3ce863ae
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-31 15:01:35 +0530

    Add CardComponent with template and styles; update AppComponent to use CardComponent

--- Day6/sample-App/src/app/app.component.ts
+++ Day6/sample-App/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { CardComponent } from './shared/card/card.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
